6b511137dc2dd52e5663a4a7b78c90e26169ce57
bilalkhan <bilal>
2020-05-24 23:49:45 +0100

    remove sample page

--- ReedExpo.Ecom.Registrations.Api.Tests/RegistrationControllerTests.cs
+++ ReedExpo.Ecom.Registrations.Api.Tests/RegistrationControllerTests.cs
@@ -14,16 +14,47 @@ namespace ReedExpo.Ecom.Registrations.Api.Tests
         [Test]
         public async Task AddRegistration_Calls_Service()
         {
+
+            //Arrange
             var mockedService = new Mock<IRegistrationsService>();
 
             var underTest = new RegistrationsController(mockedService.Object);
 
             var request = new RegistrationRequest();
 
+            //Act
             await underTest.AddRegistration(request);
 
+
+            //Assert
             mockedService.Verify(a => a.AddRegistration(request), Times.Once);
         }
 
+
+
+        [Test]
+        public async Task AddRegistration_Returns_OK()
+        {
+            //Arrange
+            var mockedService = new Mock<IRegistrationsService>();
+            var serviceResponse = new RegistrationResponse()
+            {
+                RegistrationId = Guid.NewGuid().ToString()
+            };
+
+            var request = new RegistrationRequest();
+            mockedService.Setup(a => a.AddRegistration(It.IsAny<RegistrationRequest>())).ReturnsAsync(serviceResponse);
+            var underTest = new RegistrationsController(mockedService.Object);
+
+            //Act
+            var result = await underTest.AddRegistration(request);
+
+            //Assert
+            Assert.AreEqual(result.GetType(), typeof(OkObjectResult));
+
+            
+            var actualResponse = ((result as OkObjectResult).Value) as RegistrationResponse;
+            Assert.AreEqual(actualResponse.RegistrationId, serviceResponse.RegistrationId);
+        }
     }
 }
